f5b2644697578f3c69bf872bb51ab6cb51688e90
WiFros <39113029+WiFros@users.noreply.github.com>
2023-05-11 18:54:19 +0900

    swagger-ui 의존성 추가
    localhost:8080/swagger-ui/index.html
    
    로 접속

--- src/main/java/com/lucete/template/info/repository/UserRepository.java
+++ src/main/java/com/lucete/template/info/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.stereotype.Repository;
-
 import com.lucete.template.info.model.User;
 
 @Repository
@@ -20,9 +19,24 @@ public class UserRepository {
         this.userRowMapper = new UserRowMapper();
     }
 
-    public List<User> findList(){
+    public List<User> findList() {
         // 쿼리파라미터를 넘길 필요가 없기 때문에 instance를 던짐
         // 조회된 데이터를 userRowMapper를 통해 매핑해줌
-        return namedParameterJdbcTemplate.query(UserSql.SELECT, EmptySqlParameterSource.INSTANCE, this.userRowMapper);
+        String sql = "select * from user limit 1000";
+
+
+        RowMapper<User> cityMapper = (rs, rowNum) -> {
+            User user = new User();
+            user.setName(rs.getString("name"));
+            user.setPhone(rs.getString("phone"));
+            user.setActive(rs.getInt("active"));
+            user.setTeam(rs.getString("team"));
+            user.setCreated(rs.getDate("created"));
+            user.setUpdated(rs.getDate("updated"));
+            user.setYear(rs.getInt("year"));
+            return user;
+        };
+
+        return namedParameterJdbcTemplate.query(sql, new MapSqlParameterSource(), userRowMapper);
     }
 }
