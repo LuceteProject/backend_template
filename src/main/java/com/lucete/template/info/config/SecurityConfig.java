154c687a4e6e5da548842414044139573a58db28
WiFros <39113029+WiFros@users.noreply.github.com>
2023-08-20 19:03:03 +0900

    네이버 로그인 재수정

--- src/main/java/com/lucete/template/info/config/SecurityConfig.java
+++ src/main/java/com/lucete/template/info/config/SecurityConfig.java
@@ -15,10 +15,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        //.requestMatchers("/login", "/api*", "/api-docs/**", "/swagger-ui/**").permitAll()// /swagger-ui/** 경로에 대해 인증이 필요하지 않음
-                        .anyRequest().permitAll()
+                        .requestMatchers("/login/**").permitAll() // OAuth2 로그인은 인증 없이 접근 가능
+                        .requestMatchers("/swagger-ui/**").permitAll() // Swagger UI는 인증 없이 접근 가능
+                        .requestMatchers("/api-docs/**").permitAll() // API 문서는 인증 없이 접근 가능
+                        .anyRequest().permitAll() // 모든 요청에 대해 인증이 필요
                 )
-        ;
+                .oauth2Login(withDefaults()); // OAuth2 로그인을 기본 설정으로 활성화
         return http.build();
     }
 }
