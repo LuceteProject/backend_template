d28c568537938cc88a7796f2467cc2764feefa0a
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2023-08-22 17:41:16 +0900

    csrf 부분 수정

--- src/main/java/com/lucete/template/info/config/SecurityConfig.java
+++ src/main/java/com/lucete/template/info/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/login/**").permitAll() // OAuth2 로그인은 인증 없이 접근 가능
                         .requestMatchers("/swagger-ui/**").permitAll() // Swagger UI는 인증 없이 접근 가능
