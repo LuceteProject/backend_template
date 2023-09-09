88450923d28118a99a2c2b7322928d2ca4e001b8
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2023-09-09 16:43:24 +0900

    http https cors에러 처리

--- src/main/java/com/lucete/template/info/config/CorsConfiguration.java
+++ src/main/java/com/lucete/template/info/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://lucetemusical.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
     }
 }
