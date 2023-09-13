2786a40dc849bc25c604e223dee82f887af970b9
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2023-09-13 22:13:53 +0900

    HTTPS 끝

--- src/main/java/com/lucete/template/TemplateApplication.java
+++ src/main/java/com/lucete/template/TemplateApplication.java
@@ -2,11 +2,8 @@ package com.lucete.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import java.util.Collections;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://lucetemusical.com")})
 @SpringBootApplication
 public class TemplateApplication {
 
